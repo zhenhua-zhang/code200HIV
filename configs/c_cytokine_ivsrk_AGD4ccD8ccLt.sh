d15074b5353c7b074ec2934bf1233c992a32535f
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-06-21 10:15:44 +0200

    update

--- configs/c_cytokine_ivsrk_AGD4ccD8ccLt.sh
+++ configs/c_cytokine_ivsrk_AGD4ccD8ccLt.sh
@@ -1,6 +1,6 @@
 module load R/3.3.3-foss-2015b-bare
 
-Rscript 200HIV_qtl_mapping_v0.0.2.R cknmlt -S c_cytokine_lg10tolg2_AGD4ccD8ccLt.cfg
-Rscript 200HIV_qtl_mapping_v0.0.2.R trfm -S c_cytokine_lg10tolg2_AGD4ccD8ccLt.cfg
-Rscript 200HIV_qtl_mapping_v0.0.2.R qtlmp -S c_cytokine_lg10tolg2_AGD4ccD8ccLt.cfg
-Rscript 200HIV_qtl_mapping_v0.0.2.R qtlrpt -S c_cytokine_lg10tolg2_AGD4ccD8ccLt.cfg
+Rscript 200HIV_qtl_mapping_v0.0.2.R cknmlt -S c_cytokine_ivsrk_AGD4ccD8ccLt.cfg
+Rscript 200HIV_qtl_mapping_v0.0.2.R trfm -S c_cytokine_ivsrk_AGD4ccD8ccLt.cfg
+Rscript 200HIV_qtl_mapping_v0.0.2.R qtlmp -S c_cytokine_ivsrk_AGD4ccD8ccLt.cfg
+Rscript 200HIV_qtl_mapping_v0.0.2.R qtlrpt -S c_cytokine_ivsrk_AGD4ccD8ccLt.cfg
