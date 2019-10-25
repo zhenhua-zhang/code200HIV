1f1c679b2ed6abff42abb782ce307c1cba50c481
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-10-25 18:25:59 +0200

    add dump_cvrt() function

--- covariate.py
+++ covariate.py
@@ -5,6 +5,7 @@
 #
 ## covariat.py
 #
+import os
 
 from preproc import PreProc
 
@@ -14,6 +15,23 @@ class Covariate(PreProc):
     Currently, just a wrapper of `PrePorc` as it can cover all need fucntions.
     """
 
-    def __init__(self, inpt_path, inpt_ptrn="*.tsv"):
+    def __init__(self, wk_dir, inpt_path, inpt_ptrn="*.tsv"):
         """"""
+        self.wk_dir = wk_dir
         super(Covariate, self).__init__(inpt_path, inpt_ptrn)
+
+    def dump_cvrt(self, opt_dir=None, opt_fnm="covariates.tsv"):
+        """Dump processed covariates into disk.
+        Args:
+            opt_dir (str; optional): The output directory.
+            opt_fnm (str; optional): The output file name.
+        Returns:
+            self
+        """
+
+        if opt_dir is None:
+            opt_dir = os.path.join(self.wk_dir, "Preprocessing")
+
+        self.dump_wkdtfm(opt_dir, opt_fnm)
+
+        return self
