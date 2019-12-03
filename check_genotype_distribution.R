b5f34e1159fe234d0e5e9f62727d564f2c465360
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-12-03 19:54:03 +0100

    update check_genotype_distribution.R

--- check_genotype_distribution.R
+++ check_genotype_distribution.R
@@ -1,7 +1,11 @@
 #!/usr/bin/env Rscript
+
 # TODO:
 #	1. Commandline arguments to set the splitter.
 #	2. Using command line argument parser instead of position arguments.
+
+# NOTE:
+#   1. The effect allele is encoded as 2
 
 library(ggplot2)
 library(stringr)
@@ -32,8 +36,12 @@ if (length(args) < 2) {
     target_snp <- args[3]
     phenotype_file <- args[4]
     target_phenotype <- args[5]
+} else if (length(args) == 6) {
+    target_snp <- args[3]
+    phenotype_file <- args[4]
+    target_phenotype <- args[5]
 } else {
-    warning("More than 4 arguments are given, only pick up first 4!\n")
+    warning("More than 6 arguments are given, only pick up first 6!\n")
 }
 
 genotype_dosage_file <- args[1]
@@ -85,23 +93,23 @@ print(table(target_snp_genotype))
 if (!is.null(phenotype_file)) {
     phenotype_level <- fread(phenotype_file, data.table = FALSE)
     phenotype_level <- reshape2::dcast(reshape::melt(phenotype_level, variable.name = "phenotype"), ...~id)
-    
+
     rownames(phenotype_level) <- phenotype_level$variable
     phenotype_level <- phenotype_level[, which(!colnames(phenotype_level) %in% c("phenotype"))]
-    
+
     phenotype_col_names <- colnames(phenotype_level)
     common_samples <- intersect(phenotype_col_names, genotype_col_names)
-    
+
     if (is.null(target_phenotype)){
         target_phenotype <- "prcnt_mnct"
     }
-    
+
     phenotype_level_chosen <- phenotype_level[target_phenotype, common_samples]
     target_snp_genotype_chosen <- target_snp_genotype[common_samples]
-    
+
     phenotype_genotype_chosen <- rbind(phenotype = phenotype_level_chosen, genotype = target_snp_genotype_chosen)
     phenotype_genotype_chosen["id"] <- rownames(phenotype_genotype_chosen)
-    
+
     phenotype_genotype_chosen <- reshape2::dcast(reshape2::melt(phenotype_genotype_chosen, id.vars="id"), ...~id)
     phenotype_genotype_chosen$genotype <- factor(
         phenotype_genotype_chosen$genotype,
@@ -111,24 +119,38 @@ if (!is.null(phenotype_file)) {
 
     count_per_genotype <- table(phenotype_genotype_chosen$genotype)
     genotypes <- sort(names(count_per_genotype), decreasing = (effect_allele > alternative_allele))
-    x_tick_lables <- paste0(paste(genotypes, count_per_genotype, sep="("), ")")
-    
+    x_tick_lables <- paste0(paste(genotypes, count_per_genotype, sep = "("), ")")
+
     ylabel <- str_glue("Level of {target_phenotype}")
     xlabel <- str_glue("SNP: {target_snp}; Position: chr{chrom}:{position}; Variant: {effect_allele}>{alternative_allele}")
     ftitle <- str_glue("Phenotype level per genotype")
-    
-    aov_test <- aov(phenotype~genotype, data=phenotype_genotype_chosen)
-    aov_summary <- summary.aov(aov_test)
-    print(aov_summary)
 
-    if (aov_summary[[1]]["genotype", "Pr(>F)"] <= 0.9) {
-        tukey_test <- TukeyHSD(aov_test, ordered = TRUE)
-        print(tukey_test)
+	if (!is.null(args[6])) {
+		phenotype_genotype_chosen$phenotype <- log10(as.double(phenotype_genotype_chosen$phenotype))
+	} else {
+		phenotype_genotype_chosen$phenotype <- as.double(phenotype_genotype_chosen$phenotype)
+	}
+
+    genotype_types <- unique(genotypes)
+    genotype_type_n <- length(genotype_types)
+    if (genotype_type_n == 2) {
+        print(typeof(phenotype_genotype_chosen$phenotype))
+        tt_res <- t.test(phenotype ~ genotype, data = phenotype_genotype_chosen)
+        print(tt_res)
+    } else {
+        aov_test <- aov(phenotype ~ genotype, data = phenotype_genotype_chosen)
+        aov_summary <- summary.aov(aov_test)
+        print(aov_summary)
+
+        if (aov_summary[[1]]["genotype", "Pr(>F)"] <= 0.05) {
+            tukey_test <- TukeyHSD(aov_test, ordered = TRUE)
+            print(tukey_test)
+        }
     }
-    
+
     g <- ggplot(data = phenotype_genotype_chosen) + theme_bw()
-    g <- g + geom_boxplot(aes(x=genotype, y=as.numeric(phenotype), color=genotype))
-    g <- g + geom_point(aes(x=genotype, y=as.numeric(phenotype), color=genotype))
+    g <- g + geom_boxplot(aes(x = genotype, y = phenotype, color = genotype))
+    g <- g + geom_point(aes(x = genotype, y = phenotype, color = genotype))
     g <- g + labs(title = ftitle, x = xlabel, y = ylabel)
     g <- g + scale_x_discrete(labels = x_tick_lables)
 
