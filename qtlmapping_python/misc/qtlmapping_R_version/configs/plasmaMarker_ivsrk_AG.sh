74c0919ad9b99c1302ae7b7df17bd49120612eb1
zhenhua-zhang <zhenhua.zhang217@gmail.com>
2019-12-06 10:35:14 +0100

    archive python version and permutation.R

--- qtlmapping_python/misc/qtlmapping_R_version/configs/plasmaMarker_ivsrk_AG.sh
+++
@@ -1,6 +0,0 @@
-module load R/3.3.3-foss-2015b-bare
-
-Rscript 200HIV_qtl_mapping_v0.0.2.R cknmlt -S plasmaMarker_ivsrk_AG.cfg
-Rscript 200HIV_qtl_mapping_v0.0.2.R trfm -S plasmaMarker_ivsrk_AG.cfg
-Rscript 200HIV_qtl_mapping_v0.0.2.R qtlmp -S plasmaMarker_ivsrk_AG.cfg
-Rscript 200HIV_qtl_mapping_v0.0.2.R qtlrpt -S plasmaMarker_ivsrk_AG.cfg
